5bb880eb6334802a4e7aef43cc32bd884249cf90
Akchya <cakshay201@gmail.com>
2023-12-02 00:36:49 +0530

    Skeleton + Basic functionality

---
+++ src/Components/DesciptionBox/DescriptionBox.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DesciptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="discriptionbox-navigator">
+        <div className="discriptionbox-nav-box">Description</div>
+        <div className="discriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="discriptionbox-description">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi dignissimos impedit veritatis unde architecto veniam enim optio iusto neque ea repellat aliquam consectetur, earum, dolor sed dolore, porro alias. Iure.
+          Dolorum dolores repudiandae quaerat autem accusamus molestias recusandae dignissimos, libero deleniti odio aliquam quasi neque nihil modi, praesentium ullam ad ducimus! Consequatur, libero aliquam. Atque voluptate exercitationem inventore quas laboriosam.
+          Eaque accusantium delectus modi voluptate magnam sit ipsa voluptatum porro asperiores. Molestias a, temporibus accusantium tempore facere excepturi, perspiciatis quisquam vero cupiditate nostrum, dolor odit aliquid voluptas odio mollitia itaque.
+          Sequi rerum nemo aliquam dolore, animi similique tempore dolor culpa impedit eum quibusdam libero eligendi. Enim aut corporis cumque esse tempore nemo, debitis neque amet veniam delectus fuga officia illo!
+          Maiores inventore maxime pariatur qui, repellat quasi at optio nam consequatur excepturi repudiandae soluta officiis reiciendis veritatis cupiditate ea! Magnam, ut! Odio nobis quidem quam soluta possimus consectetur libero repudiandae?
+          Nemo fugit nam saepe deserunt id reiciendis recusandae, laudantium maxime sed optio sunt at doloremque inventore, accusantium nulla hic quidem atque porro earum quod quos error. Sint cumque beatae quos.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi dignissimos impedit veritatis unde architecto veniam enim optio iusto neque ea repellat aliquam consectetur, earum, dolor sed dolore, porro alias. Iure.
+          Dolorum dolores repudiandae quaerat autem accusamus molestias recusandae dignissimos, libero deleniti odio aliquam quasi neque nihil modi, praesentium ullam ad ducimus! Consequatur, libero aliquam. Atque voluptate exercitationem inventore quas laboriosam.
+          Eaque accusantium delectus modi voluptate magnam sit ipsa voluptatum porro asperiores. Molestias a, temporibus accusantium tempore facere excepturi, perspiciatis quisquam vero cupiditate nostrum, dolor odit aliquid voluptas odio mollitia itaque.
+          Sequi rerum nemo aliquam dolore, animi similique tempore dolor culpa impedit eum quibusdam libero eligendi. Enim aut corporis cumque esse tempore nemo, debitis neque amet veniam delectus fuga officia illo!
+          Maiores inventore maxime pariatur qui, repellat quasi at optio nam consequatur excepturi repudiandae soluta officiis reiciendis veritatis cupiditate ea! Magnam, ut! Odio nobis quidem quam soluta possimus consectetur libero repudiandae?
+          Nemo fugit nam saepe deserunt id reiciendis recusandae, laudantium maxime sed optio sunt at doloremque inventore, accusantium nulla hic quidem atque porro earum quod quos error. Sint cumque beatae quos.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DesciptionBox
