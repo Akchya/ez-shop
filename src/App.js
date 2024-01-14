79524918fc70490ff5a2608b30e8039530badfd9
Akshay Chavan <cakshay201@gmail.com>
2024-01-15 00:38:20 +0530

    Testing

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSignup />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </BrowserRouter>
     </div>
   );
